3597a6280918f9a174f9d60e4a594eb54e7a017a
joao <joao182cx@gmail.com>
2019-01-29 09:53:02 -0200

    Modificação da classe Grade para usar instâncias da clase Line2D.Double para desenhar as linhas da grade.

--- src/Grade.java
+++ src/Grade.java
@@ -1,4 +1,6 @@
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
 
 import javax.swing.JPanel;
 
@@ -8,15 +10,18 @@ public class Grade extends JPanel{
 	@Override
 	public void paintComponent(Graphics g) {
 		
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
+		
 		for(int x = getWidth()/8; x < getWidth()/8 * 8; x += getWidth()/8) {
 			
-			g.drawLine(x, 0, x, getHeight());
+			g2d.draw(new Line2D.Double(x, 0, x, getHeight()));
 			
 		}
 		
 		for(int y = getHeight()/8; y < getHeight()/8 * 8; y += getHeight()/8) {
 			
-			g.drawLine(0, y, getWidth(), y);
+			g2d.draw(new Line2D.Double(0, y, getWidth(), y));
 			
 		}
 		
